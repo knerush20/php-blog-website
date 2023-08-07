513d2529efe603a716468ddeb34c147667cb40a5
karolina <karolina.nerush@gmail.com>
2023-08-07 11:32:47 +0300

    add manage buttons components to admin panel

---
+++ app/include/manage-btn-admin.php
@@ -0,0 +1,7 @@
+
+
+<div class="button row">
+    <a href=" <?= BASE_URL . "admin/$routeName/create.php"?>" class="col-2 btn btn-success">Add</a>
+    <span class="col-1"></span>
+    <a href=" <?= BASE_URL . "admin/$routeName/index.php"?>" class="col-3 btn btn-secondary">Manage</a>
+</div>
